fd394376e32d33c5180fd18843cb09824305e784
CharaD7 <jijakahn6@gmail.com>
2025-02-16 13:50:45 +0000

    Get all pdf documents
    - Get the individual urls
    - Use the last 5 characters as the file name
    - Append .pdf to each filename
    - Store in a directory called data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+from urllib.request import urlretrieve
 
 pw = sync_playwright().start()
 browser = pw.chromium.launch()
@@ -11,8 +12,14 @@ page.get_by_role("button", name="Search").nth(1).click()
 # Get all links that contain "arxiv.org/pdf" with xpath
 links = page.locator("xpath=//a[contains(@href, 'arxiv.org/pdf')]").all()
 
+# for link in links:
+#   urls = link.get_attribute("href")
+#   urlretrieve(urls, "data/" + urls[-5:] + ".pdf")
+
+urls = [ link.get_attribute("href") for link in links ]
+[ urlretrieve(url, "data/" + url[-5:] + ".pdf") for url in urls ]
+
 print(f"Page title: { page.title() }")
-print(f'PDF Links: { [ link.get_attribute("href") for link in links ] }')
 
 # close browser
 browser.close()
