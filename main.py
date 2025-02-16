d2e359be1f0210b23b42eb46358542e3ef339b51
CharaD7 <jijakahn6@gmail.com>
2025-02-16 18:33:19 +0000

    Got by-pass feature working

--- main.py
+++ main.py
@@ -13,25 +13,24 @@ pw = sync_playwright().start()
 browser = pw.chromium.launch(proxy=proxies, headless=False)
 page = browser.new_page()
 
-page.goto("https://www.arxiv.org/search")
-page.get_by_placeholder("Search term...").fill("neural network")
-page.get_by_role("button", name="Search").nth(1).click()
+page.goto("https://www.walmart.com/")
 
 # Get all links that contain "arxiv.org/pdf" with xpath
-links = page.locator("xpath=//a[contains(@href, 'arxiv.org/pdf')]").all()
+page.locator("xpath=//input[(@aria-label='Search')]").fill("testing")
+page.keyboard.press("Enter")
 
 # for link in links:
 #   urls = link.get_attribute("href")
 #   urlretrieve(urls, "data/" + urls[-5:] + ".pdf")
 
 # create the data directory if it doesn't exist
-os.makedirs("data", exist_ok=True)
+# os.makedirs("data", exist_ok=True)
 
-urls = [ link.get_attribute("href") for link in links ]
-print(f"Urls: { urls }")
-print()
-print(f"Number of Urls captured: { len(urls) }")
-[ urlretrieve(url, "data/" + url[-5:] + ".pdf") for url in urls ] or pirnt(f'Error getting { url }')
+# urls = [ link.get_attribute("href") for link in links ]
+# print(f"Urls: { urls }")
+# print()
+# print(f"Number of Urls captured: { len(urls) }")
+# [ urlretrieve(url, "data/" + url[-5:] + ".pdf") for url in urls ] or pirnt(f'Error getting { url }')
 
 # close browser
 browser.close()
