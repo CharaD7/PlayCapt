ab9e502c86fe1be6a81d1212cc8e33a7bf3c7c33
CharaD7 <jijakahn6@gmail.com>
2025-02-16 18:27:36 +0000

    Implemented proxy system for captcha by-pass

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import os
 from playwright.sync_api import sync_playwright
 from urllib.request import urlretrieve
 
+proxies = {
+  "server": "brd.superproxy.io:33335",
+  "username": "brd-customer-hl_7d0d9b18-zone-charatech_zone",
+  "password": "vx8wbsag6f84",
+}
+
 pw = sync_playwright().start()
-browser = pw.chromium.launch()
+browser = pw.chromium.launch(proxy=proxies, headless=False)
 page = browser.new_page()
 
 page.goto("https://www.arxiv.org/search")
@@ -23,9 +29,9 @@ os.makedirs("data", exist_ok=True)
 
 urls = [ link.get_attribute("href") for link in links ]
 print(f"Urls: { urls }")
+print()
+print(f"Number of Urls captured: { len(urls) }")
 [ urlretrieve(url, "data/" + url[-5:] + ".pdf") for url in urls ] or pirnt(f'Error getting { url }')
-
-print(f"Page title: { page.title() }")
 
 # close browser
 browser.close()
